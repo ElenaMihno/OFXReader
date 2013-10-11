27e2b205d6f0aa19035cfc46e21562ca18339151
renhammington <renhammington@gmail.com>
2013-10-12 09:51:03 +1300

    Initial commit

---
+++ OFXObjects.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace OFX.Objects
+{
+	[Serializable]
+	public class OFXObject
+	{
+		[XmlElement(ElementName = "SIGNONMSGSRSV1")]
+		public OFXSignOnMessageSet SignOnMessageSet { get; set; }
+
+		[XmlElement(ElementName = "CREDITCARDMSGSRSV1")]
+		public OFXCreditCardMessageSetResponseMessages CreditCardMessages { get; set; }
+		
+		[XmlElement(ElementName = "BANKMSGSRSV1")]
+		public OFXBankMessageSetResponseMessages BankMessages { get; set; }
+
+		public StatementType StatementType
+		{
+			get
+			{
+				if (CreditCardMessages != null) return StatementType.CreditCard;
+				if (BankMessages != null) return StatementType.BankAccount;
+				return StatementType.Unknown;
+			}
+		}
+
+
+		public List<OFXTransaction> Transactions
+		{
+			get
+			{
+				if (StatementType == Objects.StatementType.BankAccount)
+					return BankMessages.Response.Statement.TransactionDetails.Transactions;
+				else if (StatementType == Objects.StatementType.CreditCard)
+					return CreditCardMessages.Response.Statement.TransactionDetails.Transactions;
+				return null;
+			}
+		}
+
+		public double AvailableBalance
+		{
+			get
+			{
+				if (StatementType == Objects.StatementType.BankAccount)
+					return 0;
+				else if (StatementType == Objects.StatementType.CreditCard)
+					return CreditCardMessages.Response.Statement.AvailableBalance.Amount;
+				return 0;
+			}
+		}
+
+		public double LedgerBalance
+		{
+			get
+			{
+				if (StatementType == Objects.StatementType.BankAccount)
+					return BankMessages.Response.Statement.LedgerBalance.Amount;
+				else if (StatementType == Objects.StatementType.CreditCard)
+					return CreditCardMessages.Response.Statement.LedgerBalance.Amount;
+				return 0;
+			}
+		}
+
+
+		public double Limit
+		{
+			get
+			{
+				return LedgerBalance - AvailableBalance;
+			}
+		}
+
+
+		public void 
+
+
+	}
+
+	[Serializable]
+	public class OFXSignOnMessageSet
+	{
+		[XmlElement(ElementName = "SONRS")]
+		public OFXSignOnResponse Response { get; set; }
+
+
+	}
+
+	
+
+	
+	
+
+	#region Credit Cards
+
+	[Serializable]
+	public class OFXCreditCardMessageSetResponseMessages
+	{
+
+		[XmlElement(ElementName = "CCSTMTTRNRS")]
+		public OFXCreditCardStatementTransactionsResponse Response { get; set; }
+
+
+	}
+	
+	[Serializable]
+	public class OFXCreditCardStatementTransactionsResponse
+	{
+		[XmlElement(ElementName = "TRNUID")]
+		public string UniqueID { get; set; }
+
+		[XmlElement(ElementName = "STATUS")]
+		public OFXStatus Status { get; set; }
+
+		[XmlElement(ElementName = "CCSTMTRS")]
+		public OFXCreditCardStatementResponse Statement { get; set; }
+
+	}
+
+	[Serializable]
+	public class OFXCreditCardStatementResponse
+	{
+		[XmlElement(ElementName = "CURDEF")]
+		public string Currency { get; set; }
+
+		[XmlElement(ElementName = "CCACCTFROM")]
+		public OFXCreditCardAccountDetails AccountDetails { get; set; }
+
+		[XmlElement(ElementName = "LEDGERBAL")]
+		public OFXBalance LedgerBalance { get; set; }
+
+		[XmlElement(ElementName = "AVAILBAL")]
+		public OFXBalance AvailableBalance { get; set; }
+
+		[XmlElement(ElementName = "BANKTRANLIST")]
+		public OFXTransactionList TransactionDetails { get; set; }
+
+
+	}
+
+	[Serializable]
+	public class OFXCreditCardAccountDetails
+	{
+		[XmlElement(ElementName = "ACCTID")]
+		public string AccountNumber { get; set; }
+	}
+	#endregion
+
+	#region Bank Accounts
+
+	[Serializable]
+	public class OFXBankMessageSetResponseMessages
+	{
+
+		[XmlElement(ElementName = "STMTTRNRS")]
+		public OFXBankStatementTransactionsResponse Response { get; set; }
+
+
+	}
+
+	[Serializable]
+	public class OFXBankStatementTransactionsResponse
+	{
+		[XmlElement(ElementName = "TRNUID")]
+		public string UniqueID { get; set; }
+
+		[XmlElement(ElementName = "STATUS")]
+		public OFXStatus Status { get; set; }
+
+		[XmlElement(ElementName = "STMTRS")]
+		public OFXBankStatementResponse Statement { get; set; }
+
+	}
+
+	[Serializable]
+	public class OFXBankStatementResponse
+	{
+		[XmlElement(ElementName = "CURDEF")]
+		public string Currency { get; set; }
+
+		[XmlElement(ElementName = "BANKACCTFROM")]
+		public OFXBankAccountDetails AccountDetails { get; set; }
+
+		[XmlElement(ElementName = "LEDGERBAL")]
+		public OFXBalance LedgerBalance { get; set; }
+
+		[XmlElement(ElementName = "BANKTRANLIST")]
+		public OFXTransactionList TransactionDetails { get; set; }
+
+
+	}
+
+	[Serializable]
+	public class OFXBankAccountDetails
+	{
+		[XmlElement(ElementName = "ACCTID")]
+		public string AccountNumber { get; set; }
+
+		[XmlElement(ElementName = "ACCTTYPE")]
+		public string Type { get; set; }
+
+		[XmlElement(ElementName = "BANKID")]
+		public string BankID { get; set; }
+	}
+
+	#endregion
+
+	#region Generic
+
+	public enum StatementType
+	{
+		CreditCard,
+		BankAccount,
+		Unknown
+	}
+
+
+	[Serializable]
+	public class OFXStatus
+	{
+		[XmlElement(ElementName = "CODE")]
+		public string Code { get; set; }
+
+		[XmlElement(ElementName = "SEVERITY")]
+		public string Severity { get; set; }
+	}
+
+	[Serializable]
+	public class OFXSignOnResponse
+	{
+		[XmlElement(ElementName = "DTSERVER")]
+		public string ServerDate { get; set; }
+
+		[XmlElement(ElementName = "DTPROFUP")]
+		public string LastProfileUpdateDate { get; set; }
+
+		[XmlElement(ElementName = "DTACCTUP")]
+		public string LastAccountUpdate { get; set; }
+
+		[XmlElement(ElementName = "LANGUAGE")]
+		public string Language { get; set; }
+
+		[XmlElement(ElementName = "STATUS")]
+		public OFXStatus Status { get; set; }
+	}
+
+	[Serializable]
+	public class OFXBalance
+	{
+		[XmlElement(ElementName = "BALAMT")]
+		public double Amount { get; set; }
+
+		[XmlElement(ElementName = "DTASOF")]
+		public string EffectiveDate { get; set; }
+	}
+
+	[Serializable]
+	public class OFXTransactionList
+	{
+		[XmlElement(ElementName = "DTSTART")]
+		public string StartDate { get; set; }
+
+		[XmlElement(ElementName = "DTEND")]
+		public string EndDate { get; set; }
+
+		[XmlArray("TRANSACTIONS")]
+		[XmlArrayItem("STMTTRN")]
+		public List<OFXTransaction> Transactions { get; set; }
+	}
+
+
+	[Serializable]
+	public class OFXTransaction
+	{
+		[XmlElement(ElementName = "TRNTYPE")]
+		public string Type { get; set; }
+
+		[XmlElement(ElementName = "DTPOSTED")]
+		public string DatePosted { get; set; }
+
+		[XmlElement(ElementName = "DTUSER")]
+		public string DateUser { get; set; }
+
+		[XmlElement(ElementName = "TRNAMT")]
+		public double Amount { get; set; }
+
+		[XmlElement(ElementName = "FITID")]
+		public string TransactionID { get; set; }
+
+		[XmlElement(ElementName = "REFNUM")]
+		public string ReferenceNumber { get; set; }
+
+		[XmlElement(ElementName = "NAME")]
+		public string Name { get; set; }
+
+		[XmlElement(ElementName = "MEMO")]
+		public string Memo { get; set; }
+
+	}
+
+	#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using OFX.Objects;
+
+
+namespace OFX
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FixStatement("anz");
+			FixStatement("bankdirect");
+			FixStatement("homeloan");
+			FixStatement("topup");
+			Console.ReadLine();
+
+		}
+
+		public static void FixStatement(string prefix)
+		{
+			StringBuilder output = new StringBuilder();
+			StreamReader file = new StreamReader("C:\\projects\\ofx\\ofx\\" + prefix + ".ofx");
+			string line;
+
+			Console.WriteLine("=============================================================");
+			Console.WriteLine(prefix);
+			Console.WriteLine();
+
+			while ((line = file.ReadLine()) != null)
+			{
+				// Get past the header
+				if (line.StartsWith("<") == false) continue;
+
+				string openingTag = line.Substring(1, line.IndexOf(">") - 1);
+				string closingTag = string.Empty;
+
+				int openingTags = line.Count(r => r == '<');
+				if (openingTags > 1)
+				{
+					// There are two tags on this line
+					// Check that both are equal
+					int length = line.Length;
+					int lastOpeningMarkerIndex = line.LastIndexOf("<");
+					closingTag = line.Substring(lastOpeningMarkerIndex + 1, length - lastOpeningMarkerIndex - 2);
+					if (closingTag.StartsWith("/")) closingTag = closingTag.Substring(1);
+
+					if (closingTag != openingTag)
+					{
+						line = line.Insert(lastOpeningMarkerIndex, "</" + openingTag + ">");
+					}
+				}
+				else if (line.EndsWith(">") == false)
+				{
+					line += "</" + openingTag + ">";
+				}
+				
+				output.Append(line);
+
+
+			}
+			file.Close();
+
+
+			// Fix bad xml
+			output = WrapTransactions(output);
+
+			File.WriteAllText(@"c:\projects\ofx\ofx\" + prefix + ".xml", output.ToString());
+
+			XmlRootAttribute xRoot = new XmlRootAttribute();
+			xRoot.ElementName = "OFX";
+			xRoot.IsNullable = true;
+
+			var serializer = new XmlSerializer(typeof(OFXObject), xRoot);
+			serializer.UnknownElement += new XmlElementEventHandler(helper_UnknownElement);
+
+			OFXObject statement;
+
+			using (TextReader reader = new StringReader(output.ToString()))
+			{
+				statement = (OFXObject)serializer.Deserialize(reader);
+			}
+
+
+
+			//Console.WriteLine("------------------------------------------------");
+			//Console.WriteLine(bankStatement.SignOnMessageSet.Response.Status.Code);
+			//Console.WriteLine(bankStatement.SignOnMessageSet.Response.ServerDate);
+			//Console.WriteLine(bankStatement.SignOnMessageSet.Response.Language);
+			//Console.WriteLine("Available balance: " + bankStatement.CreditCardMessages.Response.Statement.AvailableBalance.Amount);
+			//Console.WriteLine("Ledger balance: " + bankStatement.CreditCardMessages.Response.Statement.LedgerBalance.Amount);
+			//Console.WriteLine("From: " + bankStatement.CreditCardMessages.Response.Statement.TransactionDetails.StartDate);
+			//Console.WriteLine("To: " + bankStatement.CreditCardMessages.Response.Statement.TransactionDetails.EndDate);
+			//Console.WriteLine("------------------------------------------------");
+			//Console.WriteLine("Transactions: " + bankStatement.CreditCardMessages.Response.Statement.TransactionDetails.Transactions.Count.ToString());
+			//Console.WriteLine("Start: " + bankStatement.CreditCardMessages.Response.Statement.TransactionDetails.StartDate);
+			//Console.WriteLine("End: " + bankStatement.CreditCardMessages.Response.Statement.TransactionDetails.EndDate);
+			//Console.WriteLine("------------------------------------------------");
+
+			List<OFXTransaction> transactions = statement.Transactions;
+			//if (statement.StatementType == StatementType.CreditCard)
+			//    transactions = statement.CreditCardMessages.Response.Statement.TransactionDetails.Transactions;
+			//else if (statement.StatementType == StatementType.BankAccount)
+			//    transactions = statement.BankMessages.Response.Statement.TransactionDetails.Transactions;
+
+			//transactions.ForEach(transaction =>
+			//{
+			//    Console.WriteLine("ID: " + transaction.TransactionID.ToString());
+			//    Console.WriteLine("Type: " + transaction.Type.ToString());
+			//    Console.WriteLine("Amount: " + transaction.Amount.ToString());
+			//    Console.WriteLine("Name: " + transaction.Name);
+			//    Console.WriteLine("------------------------------------------------");
+			//});
+
+			Console.WriteLine("------------------------------------------------");
+			Console.WriteLine("PAYEE SUMMARY");
+			Console.WriteLine("------------------------------------------------");
+
+			var query = (from t in transactions
+						group t by t.Name into k
+						select new { Payee = k.Key, Total = k.Sum(p=>p.Amount) })
+						.OrderByDescending(i=>i.Total);
+
+
+			query.ToList().ForEach(q =>
+			{
+				Console.WriteLine(q.Payee.PadRight(35) + ": " + q.Total.ToString("C"));
+
+			});
+
+			var query2 = (from t in transactions
+						 group t by t.Type into k
+						 select new { Type = k.Key, Total = k.Sum(p=>p.Amount) })
+						 .OrderByDescending(i=>i.Total);
+
+			Console.WriteLine("------------------------------------------------");
+			Console.WriteLine("INCOME vs SPENDING");
+			Console.WriteLine("------------------------------------------------");
+
+			query2.ToList().ForEach(q =>
+			{
+				Console.WriteLine(q.Type.PadRight(35) + ": " + q.Total.ToString("C"));
+			});
+			Console.WriteLine("BALANCE".PadRight(35) + ": " + statement.LedgerBalance.ToString("C"));
+			Console.WriteLine("AVAILABLE".PadRight(35) + ": " + statement.AvailableBalance.ToString("C"));
+			Console.WriteLine("LIMIT".PadRight(35) + ": " + statement.Limit.ToString("C"));
+		}
+
+		
+
+		// Repair badly formated xml for transaction list
+		private static StringBuilder WrapTransactions(StringBuilder output)
+		{
+
+			// Find the first STMTTRN
+			// Place an opener before it
+			// Find the last STMTTRN
+			// Place a closer after it
+			output.Insert(output.ToString().IndexOf("<STMTTRN>"), "<TRANSACTIONS>");
+			string lastTransactionClosingTag = "</STMTTRN>";
+			output.Insert(output.ToString().LastIndexOf(lastTransactionClosingTag) + lastTransactionClosingTag.Length, "</TRANSACTIONS>");
+
+			return output;
+		}
+
+		public static void helper_UnknownElement(object sender, XmlElementEventArgs e)
+		{
+			Console.WriteLine("Unknown element found");
+			Console.WriteLine("Line: " + e.LineNumber.ToString());
+			Console.WriteLine("Element name: " + e.Element.Name);
+		}
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
